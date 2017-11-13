38c519ed751919a0fafaea81a8f95a5ea7726bd9
Jasper <chcifung@gmail.com>
2017-11-13 20:22:59 +0800

    add the v-model v-for v-bind

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-var app = new Vue({
-	el:'#app',
+var container= new Vue({
+	el: "#container",
 	data:{
-		name:'chcifung',
-		age: 13,
-		sex: "girl",
+		url:"http://readhub.me",
+		img: "http://g.hiphotos.baidu.com/image/h%3D300/sign=827213cf3afa828bce239be3cd1e41cd/0eb30f2442a7d933f6d973b9a44bd11372f00182.jpg",
+		cls: "btn btn-default"
 	}
 })
