f143587274de9dc9a907b4f299a4047df188e045
chcifung <chcifung@gamil.com>
2017-11-14 14:15:56 +0800

    new add the v-on v-bind v-model.lazy.number.trim

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 var container= new Vue({
 	el: "#container",
+	methods:{
+		onClick:function(){
+			console.log("mouose click");
+		},
+		onEnter:function(){
+			console.log('mouse enter');
+		},
+		onOut:function(){
+			console.log("mouse leave");
+		},
+		onSubmit:function(){
+			console.log("submit");
+		},
+		keyUp:function(){
+			console.log('keyup');
+		}
+	},
 	data:{
+		sex: 'male',
+		name: "ff",
 		url:"http://readhub.me",
 		img: "http://g.hiphotos.baidu.com/image/h%3D300/sign=827213cf3afa828bce239be3cd1e41cd/0eb30f2442a7d933f6d973b9a44bd11372f00182.jpg",
 		cls: "btn btn-default"
