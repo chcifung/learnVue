b51c6535a7de944d06655af70641ad56b3106791
Jasper <chcifung@gmail.com>
2017-11-14 21:54:06 +0800

    add the component

--- js/main.js
+++ js/main.js
@@ -1,30 +1,14 @@
 var container= new Vue({
 	el: "#container",
-	methods:{
-		onClick:function(){
-			console.log("mouose click");
-		},
-		onEnter:function(){
-			console.log('mouse enter');
-		},
-		onOut:function(){
-			console.log("mouse leave");
-		},
-		onSubmit:function(){
-			console.log("submit");
-		},
-		keyUp:function(){
-			console.log('keyup');
+	components: {
+		'alert':{
+			template: '<button @click="on_click">贪贪贪</button>',
+			methods:{
+			on_click:function(){
+				alert('a');
+				}
+					}
 		}
-	},
-	data:{
-		hometown: "beijing",
-		work: [],
-		article:"我认为还是佛is金佛生逢乱世舒服舒服就好",
-		sex: [],
-		name: "ff",
-		url:"http://readhub.me",
-		img: "http://g.hiphotos.baidu.com/image/h%3D300/sign=827213cf3afa828bce239be3cd1e41cd/0eb30f2442a7d933f6d973b9a44bd11372f00182.jpg",
-		cls: "btn btn-default"
-	}
-})
+		
+	}		
+});
