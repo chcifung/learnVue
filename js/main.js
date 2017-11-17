58315e7f84891dfabc3475827ae0f9c1f46119d0
Jasper <chcifung@gmail.com>
2017-11-17 21:15:38 +0800

    add the parents and child component

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+Vue.component('balance',{
+	template:
+	`
+	<div>
+		<show @show-balance="show_balance"></show>
+		<div v-if="show">
+		您的余额：38.22元</div>
+	</div>
+	`
+	,
+	methods:{
+		show_balance:function(data){
+			this.show=true;
+			console.log(data);
+		}
+	},
+	data:function(){
+		return {
+			show: false,
+		};
+	}
+});
+
+Vue.component('show',{
+	template: "<button @click='on_click()'>显示余额</button>",
+	methods:{
+		on_click:function(){
+			this.$emit('show-balance',{a:1,b:2});
+		}
+	}
+})
+
 Vue.component('like',{
 	template:"#like-component-tpl",
 	data:function(){
