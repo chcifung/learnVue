9e8de67ca8906688b69964bdf8d4379a7f6bfb02
Jasper <chcifung@gmail.com>
2017-11-18 19:44:16 +0800

    add the slot function template

--- js/main.js
+++ js/main.js
@@ -1,52 +1,7 @@
-var base = {
-	methods:{
-		show:function(){
-			this.visible = true;
-		},
-		hide:function(){
-			this.visible = false;
-		},
-		toggle:function(){
-			this.visible = !this.visible;
-		},
-	},
-	data:function(){
-		return {
-			visible: false,
-		}
-	},
-}
-
-
-Vue.component('tooltip',{
-	template:`
-	<div>
-		<span @mouseenter="show" @mouseleave="hide">354</span>
-		<div v-if="visible">
-		今后捷豹路虎
-一定会与中国公司百度、阿里巴巴
-		</div>
-	</div>`,
-	mixins:[base],
-	data:function(){
-		return {
-			visible:true,
-		}
-	}
-	
-});
-Vue.component('popup',{
-	template: `<div>
-		<button @click="toggle">显示或隐藏</button>
-		<div v-if="visible">
-			<h4>3636</h4>
-			<p>今后捷豹路虎
-一定会与中国公司百度、阿里巴巴和腾讯（下称 BAT）合作 ... 在中国，捷豹路虎计划引入路虎揽胜运动版插电混合动力车型，并且会在 2019 年左右推出本土化生产新能源产品 ... 他透露，捷豹路虎不会像大众、福特等公司一样另寻合作伙伴，而是会参与现有合作伙伴奇瑞汽车正在进行的新能源项目。</p>
-		</div>
-	</div>`,
-	mixins:[base],
-});
+Vue.component('panel',{
+	template:"#panel-tpl",
+})
 
 new Vue({
 	el:"#container",
-});
+})
