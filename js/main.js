9d73f539ae2293df240859eeba483752f868161a
Jasper <chcifung@gmail.com>
2017-11-18 16:39:11 +0800

    add the 18.13  and event mounted

--- js/main.js
+++ js/main.js
@@ -1,53 +1,38 @@
-Vue.component('balance',{
-	template:
-	`
-	<div>
-		<show @show-balance="show_balance"></show>
-		<div v-if="show">
-		您的余额：38.22元</div>
-	</div>
-	`
-	,
-	methods:{
-		show_balance:function(data){
-			this.show=true;
-			console.log(data);
+var Event = new Vue();
+
+
+Vue.component('huahua',{
+	template:`
+	<div>我说的是<input @keyup="onchange()" v-model="isaid"> {{isaid}}</div>`,
+	data: function(){
+		return {
+			isaid:'',
 		}
 	},
-	data:function(){
-		return {
-			show: false,
-		};
-	}
+	methods:{
+		onchange:function(){
+			Event.$emit('huahua-said-something',this.isaid);
+		}
+	},
 });
 
-Vue.component('show',{
-	template: "<button @click='on_click()'>显示余额</button>",
-	methods:{
-		on_click:function(){
-			this.$emit('show-balance',{a:1,b:2});
-		}
-	}
-})
-
-Vue.component('like',{
-	template:"#like-component-tpl",
+Vue.component('shuandan',{
+	template: `
+		<div>
+		花花说：{{huahuasaid}}
+		</div>
+			`,
 	data:function(){
 		return {
-			like_count:10,
-			liked:false,
-		}
+			huahuasaid: '',
+		};
 	},
-	methods: {
-		toggle_like:function(){
-			if(!this.liked){
-				this.like_count++;
-			}else{
-				this.like_count--;
-			};
-			this.liked=!this.liked;
-			
-		}
+	mounted: function(){
+		var me = this;
+		Event.$on('huahua-said-something',function(data){
+			console.log(data);
+			me.huahuasaid = data;
+		})
 	}
 });
 
