26ad76b1254a4ca5d023b9613ea3d2619200d794
Jasper <chcifung@gmail.com>
2017-11-20 20:34:46 +0800

    add the children router and router-view

--- js/main.js
+++ js/main.js
@@ -26,10 +26,25 @@ var routes = [
 				<div>
 					<h1>大家好，我叫{{$route.params.name}}</h1>
 					<p>我今年{{$route.query.age}}岁了</p>
+					<router-link to="more" append>更多信息</router-link>
+					<router-view></router-view>
 				</div>
 				`
+		},
+		children: [
+		{
+			path:'more',
+			component:{
+				template:`
+					<div>
+					用户：{{$route.params.name}}的详细信息
+					饭还哦发货速度发货速度就发货速度加快和好的说法个电话关机的咖啡馆货到付款</div>
+				`
+			}
 		}
-	}
+		],
+	},
+	
 ];
 
 var router = new VueRouter({
