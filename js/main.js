46c3991a25727eb055aadcd3dc775c2bef11cd2f
Jasper <chcifung@gmail.com>
2017-11-20 20:07:29 +0800

    add the vue router params and get query params

--- js/main.js
+++ js/main.js
@@ -1,7 +1,42 @@
-Vue.component('panel',{
-	template:"#panel-tpl",
-})
+var routes = [
+	{
+		path:'/',
+		component:{
+			template:`
+				<div>
+					<h1>HOME</h1>
+				</div>
+				`
+		}
+	},
+	{
+		path:'/about',
+		component:{
+			template:`
+				<div>
+					<h1>About US</h1>
+				</div>
+				`
+		}
+	},
+	{
+		path:'/user/:name',
+		component:{
+			template:`
+				<div>
+					<h1>大家好，我叫{{$route.params.name}}</h1>
+					<p>我今年{{$route.query.age}}岁了</p>
+				</div>
+				`
+		}
+	}
+];
+
+var router = new VueRouter({
+	routes: routes,
+});
 
 new Vue({
-	el:"#container",
+	el: "#container",
+	router : router,
 })
